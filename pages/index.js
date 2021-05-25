9e89c8b01c7aaae00a9a95e4ebd7ec3e3ce75b19
p-alex <alexey9021@gmail.com>
2021-05-25 21:57:43 +0300

    Now you are no longer able to display 2 or more operators in a row

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import { themeOne, themeTwo, themeThree, GlobalStyles } from "../themes/themes";
 import Head from "next/head";
@@ -11,32 +11,35 @@ export default function Home() {
   let [calc, setCalc] = useState("");
   let [theme, setTheme] = useState("theme-one");
 
+  useEffect(() => {
+    if (/(\x|\/|\+|\-|\.){2,}/.test(calc)) {
+      setCalc(calc.slice(0, calc.length - 1));
+    }
+  }, [calc]);
+
   const handleThemeChange = (theme) => setTheme(theme);
 
   const handleCalc = (char) =>
-    calc.length < 14 ? setCalc((calc = calc + char)) : null;
+    calc.length < 14 ? setCalc((calc += char)) : null;
 
   const handleReset = () => setCalc("");
 
   const handleResult = () => {
     if (
-      !/(\*|\/|\+|\-)/.test(calc.slice(calc.length - 1, calc.length)) &&
-      !/(\*|\/)/.test(calc.slice(0, 1)) &&
-      !/([\*|\+|\-|\/|\.]{2,})/.test(calc.slice(1, calc.length - 1)) &&
-      !/(\+|\-|\*|\/)0{1,}[1-9]{1,}/g.test(calc) &&
+      !/(\x|\/|\+|\-)/.test(calc.slice(calc.length - 1, calc.length)) &&
+      !/(\x|\/)/.test(calc.slice(0, 1)) &&
+      !/(\+|\-|\x|\/)0{1,}[1-9]{1,}/g.test(calc) &&
       !/^0[0-9]{1,}/.test(calc) &&
-      !/\.(\+|\-|\*|\/){1,}/.test(calc) &&
-      calc.length
-    ) {
-      setCalc(eval(calc.replace("x", "*")).toString());
-      return;
-    }
+      !/\.(\+|\-|\x|\/){1,}/.test(calc) &&
+      calc.length &&
+      calc !== "."
+    )
+      return setCalc(eval(calc.replaceAll("x", "*")).toFixed(5).toString());
     return setCalc("");
   };
 
-  const handleDelete = () => {
+  const handleDelete = () =>
     calc.length !== 1 ? setCalc(calc.slice(0, calc.length - 1)) : setCalc("");
-  };
 
   return (
     <>
